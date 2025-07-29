f448a26c86e2f42cc8db79532f62383b49b51bb9
Derin <aderinsolasamashimi@gmail.com>
2025-07-29 19:34:45 +0100

    third commit

--- app/components/CreateAcct/CreateAcct.tsx
+++ app/components/CreateAcct/CreateAcct.tsx
@@ -20,8 +20,15 @@ const CreateAcct = () => {
     const router = useRouter();
 
     const handleCreateAccount = async (e: React.FormEvent<HTMLFormElement>) => {
-
         e.preventDefault();
+        if (password.length < 8) {
+            const el = document.getElementById("wrong")!
+            if (el) el.classList.remove("hidden");
+            return
+        }
+        const el = document.getElementById("wrong");
+        if (el) el.classList.add("hidden");
+
         try {
             await createUserWithEmailAndPassword(auth, email, password).then(() => {
                 addDoc(collection(db, 'users'), {
@@ -45,6 +52,7 @@ const CreateAcct = () => {
         try {
             const result = await signInWithPopup(auth, provider)
             const user = result.user
+            router.push('/components/Login')
             console.log(user)
         } catch (error) {
             if (typeof error === 'object' && error !== null && 'code' in error) {
@@ -55,12 +63,12 @@ const CreateAcct = () => {
                     console.error('Google Sign-In Error:', err)
                 }
             }
-         } finally {
-                setSigningIn(false)
-            }
+        } finally {
+            setSigningIn(false)
         }
-        return (
-             <main
+    }
+    return (
+        <main
             className="xl:flex w-screen h-screen overflow-hidden"
             style={{ fontFamily: 'var(--font-nunito), sans-serif' }}
         >
@@ -75,78 +83,69 @@ const CreateAcct = () => {
             </section>
             <section className="flex justify-center xl:ml-[45%] w-full overflow-y-auto h-screen px-[20px] xl:px-[50px] ">
                 <section className="w-full max-w-[440px] xl:min-w-[600px] xl:max-w-[700px] mt-[64px]">
-                                <div className='w-full flex items-center justify-center'>
-                                    <Image src={Logo} alt="Neuroloom Logo" className='' />
-                                </div>
-                        <div className='w-full mt-[29px] text-center'>
-                            <div>
-                                <p style={{ fontWeight: 700 }} className='w-full text-[24px]'>Create an account</p>
-                                <p style={{ fontWeight: 400 }} className='text-[16px] text-[#6F6F6F]'>Fill in the field below to create an account</p>
-                            </div>
-                            <form className='w-full mt-[30px]' onSubmit={(e) => (handleCreateAccount(e))}>
-                                <div className='w-full'>
-                                    <label htmlFor='name' className='text-left block' style={{ fontWeight: 500 }}>Full Name</label>
-                                    <input type="text"
-                                        name='name'
-                                        placeholder='John Doe'
-                                        className='border border-[#D9D9D9] focus:border-[#00BFA5]
+                    <div className='w-full flex items-center justify-center'>
+                        <Image src={Logo} alt="Neuroloom Logo" className='' />
+                    </div>
+                    <div className='w-full mt-[29px] text-center'>
+                        <div>
+                            <p style={{ fontWeight: 700 }} className='w-full text-[24px]'>Create an account</p>
+                            <p style={{ fontWeight: 400 }} className='text-[16px] text-[#6F6F6F]'>Fill in the field below to create an account</p>
+                        </div>
+                        <form className='w-full mt-[30px]' onSubmit={(e) => (handleCreateAccount(e))}>
+                            <div className='w-full'>
+                                <label htmlFor='name' className='text-left block' style={{ fontWeight: 500 }}>Full Name</label>
+                                <input type="text"
+                                    name='name'
+                                    placeholder='John Doe'
+                                    className='border border-[#D9D9D9] focus:border-[#00BFA5]
                                            rounded-xl p-[12px] w-[100%] mb-[30px]'
-                                        onChange={(e) => setName(e.target.value)}
-                                    />
-                                </div>
-                                <div className='w-full'>
-                                    <label htmlFor='email' className='text-left block' style={{ fontWeight: 500 }}>Email Address</label>
-                                    <input type="email"
-                                        name='email'
-                                        placeholder='name@example.com '
-                                        className='border border-[#D9D9D9] focus:border-[#00BFA5]
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            </div>
+                            <div className='w-full'>
+                                <label htmlFor='email' className='text-left block' style={{ fontWeight: 500 }}>Email Address</label>
+                                <input type="email"
+                                    name='email'
+                                    placeholder='name@example.com '
+                                    className='border border-[#D9D9D9] focus:border-[#00BFA5]
              not-placeholder-shown:border-[#00BFA5] rounded-xl p-[12px] w-[100%] mb-[30px]'                  onChange={(e) => setEmail(e.target.value)}
-                                    />
-                                </div>
-                                <div>
-                                    <label htmlFor='password' className='text-left block' style={{ fontWeight: 500 }}>Password</label>
-                                    <input type="password"
-                                        name='password'
-                                        placeholder='min. of 8 characters'
-                                        className='border border-[#D9D9D9]
+                                />
+                            </div>
+                            <div className=''>
+                                <label htmlFor='password' className='text-left block' style={{ fontWeight: 500 }}>Password</label>
+                                <input type="password"
+                                    name='password'
+                                    placeholder='min. of 8 characters'
+                                    className='border border-[#D9D9D9]
                                      focus:border-[#00BFA5]
                                      not-placeholder-shown:border-[#00BFA5] 
-                               rounded-xl p-[12px] w-full mb-[12px]'
-                                        onChange={(e) => setPassword(e.target.value)}
-                                    />
-                                </div>
-                                <div className='flex justify-between mb-[30px]'>
-                                    <div className='flex'>
-                                        <label htmlFor='remember' className='text-left flex items-center cursor-pointer'>
-                                            <input type="checkbox" name='remember' className='mr-[8px] accent-[#00BFA5] hover:cursor-pointer' />
-                                            <span>Remember me</span></label>
-                                    </div>
-                                    <div className='hover:cursor-pointer'>
-                                        Forgot password?
-                                    </div>
-                                </div>
-                                <button className='bg-[#00BFA5] text-white rounded-xl py-[12px] w-full hover:cursor-pointer' type='submit'>Sign up</button>
-                                <div className='mt-[12px] mb-[15px]'>
-                                    <p className='text-center '>Already have an account? <Link href="/components/Login" className='text-[#00BFA5] hover:cursor-pointer'>Log in</Link></p>
-                                </div>
-                                <section className='flex items-center gap-4 mb-[12px]'>
-                                    <hr className='flex-grow border-t border-[#CCCCCC]' />
-                                    <span className=''>or</span>
-                                    <hr className='flex-grow border-t border-[#CCCCCC]' />
-                                </section>
-                            </form>
+                               rounded-xl p-[12px] w-full '
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
+                            <div id='wrong' className='hidden text-red-600 '>Password cannot be less than 8 characters</div>
+                            <button className='bg-[#00BFA5] text-white rounded-xl py-[12px] w-full hover:cursor-pointer mt-[50px]' type='submit'>Sign up</button>
+                            <div className='mt-[12px] mb-[15px]'>
+                                <p className='text-center '>Already have an account? <Link href="/components/Login" className='text-[#00BFA5] hover:cursor-pointer'>Log in</Link></p>
+                            </div>
+                            <section className='flex items-center gap-4 mb-[12px]'>
+                                <hr className='flex-grow border-t border-[#CCCCCC]' />
+                                <span className=''>or</span>
+                                <hr className='flex-grow border-t border-[#CCCCCC]' />
+                            </section>
+                        </form>
 
-                            <button className=' border border-[#CCCCCC] rounded-xl py-[12px] w-full hover:cursor-pointer' onClick={() => handleGoogleSignup()} disabled={signingIn}>
-                                <div className='flex items-center justify-center gap-[4px]'>
-                                    <Image src={Google} alt='Google logo' />
-                                    <p>Sign in with Google</p>
-                                </div>
-                            </button>
-                        </div>
-                    </section>
+                        <button className=' border border-[#CCCCCC] rounded-xl py-[12px] w-full hover:cursor-pointer' onClick={() => handleGoogleSignup()} disabled={signingIn}>
+                            <div className='flex items-center justify-center gap-[4px]'>
+                                <Image src={Google} alt='Google logo' />
+                                <p>Sign in with Google</p>
+                            </div>
+                        </button>
+                    </div>
                 </section>
-            </main>
-        )
-    }
+            </section>
+        </main>
+    )
+}
 
-    export default CreateAcct
+export default CreateAcct
--- app/components/Login/Login.tsx
+++ app/components/Login/Login.tsx
@@ -119,60 +119,7 @@ const Login = () => {
             </section>
         </main>
 
-        // <main className='md:flex md:grid-cols-[2fr 3fr] w-screen relative  select-none h-screen' style={{ fontFamily: 'var(--font-nunito), sans-serif' }}>
-        //     <section className='hidden  md:block basis-4/9 h-full overflow-hidden'>
-        //         <Image src={Picture} width={638} alt='' className='w-full h-full object-cover scale-120' />
-        //     </section>
-        //     <section className='flex justify-center mx-[20px] md:mx-[50px] basis-full md:basis-5/9 '>
-        //         <section className='w-full max-w-[440px] md:min-w-[600px] md:max-w-[700px] mt-[64px] '>
-        //             <div className='w-full flex items-center justify-center'>
-        //                 <Image src={Logo} alt="Neuroloom Logo" className='' />
-        //             </div>
-        //             <div className='w-full mt-[29px] text-center'>
-        //                 <div>
-        //                     <p style={{ fontWeight: 700 }} className='w-full text-[24px]'>Log in to your account</p>
-        //                     <p style={{ fontWeight: 400 }} className='text-[16px] text-[#6F6F6F]'>Fill in the field below to log in</p>
-        //                 </div>
-        //                 <form className='w-full mt-[30px]' onSubmit={(e) => (handleSignin(e))}>
-        //                     <div className='w-full'>
-        //                         <label htmlFor='email' className='text-left block md:mb-[12px]' style={{ fontWeight: 500 }}>Email Address</label>
-        //                         <input type="email" 
-        //                         name='email' 
-        //                         placeholder='name@example.com ' 
-        //                         className='border border-[#D9D9D9] focus:border-[#00BFA5]
-        //      not-placeholder-shown:border-[#00BFA5] rounded-xl md:rounded-2xl p-[12px] md:p-[20px] w-[100%] mb-[30px]' 
-        //                         onChange={(e) => setEmail(e.target.value)}/>
-        //                     </div>
-        //                     <div>
-        //                         <label htmlFor='password' className='text-left block md:mb-[12px]' style={{ fontWeight: 500 }}>Password</label>
-        //                         <input type="password"
-        //                          name='password' 
-        //                          placeholder='min. of 8 characters' 
-        //                          className='border border-[#D9D9D9]
-        //                              focus:border-[#00BFA5]
-        //                              not-placeholder-shown:border-[#00BFA5] 
-        //                        rounded-xl md:rounded-2xl p-[12px] w-full mb-[12px] md:p-[20px]'
-        //                             onChange={(e) => setPassword(e.target.value)} />
-        //                     </div>
-        //                     <div className='flex justify-between mb-[30px]'>
-        //                         <div className='flex'>
-        //                             <label htmlFor='remember' className='text-left flex items-center cursor-pointer'>
-        //                                 <input type="checkbox" name='remember' className='mr-[8px] accent-[#00BFA5] hover:cursor-pointer' />
-        //                                 <span>Remember me</span></label>
-        //                         </div>
-        //                         <div className='hover:cursor-pointer'>
-        //                             Forgot password?
-        //                         </div>
-        //                     </div>
-        //                 <button className='bg-[#00BFA5] text-white rounded-xl md:rounded-2xl py-[12px] w-full md:py-[20px] text-xl hover:cursor-pointer' type='submit'>Log In</button>
-        //                     <div className='mt-[12px]'>
-        //                         <p className='text-center '>Don&apos;t have an account? <Link href="/components/CreateAcct" className='text-[#00BFA5] hover:cursor-pointer'>Sign up</Link></p>
-        //                     </div>
-        //                 </form>
-        //             </div>
-        //         </section>
-        //     </section>
-        // </main>
+        
     )
 }
 
